832ce70d89254964ed769a4b04276e5a2017563d
rubenanko <rubenanko@gmail.com>
2025-10-13 16:26:09 +0200

    feat(): list, and filter by tags

--- src/modules/core-modules/list.py
+++ src/modules/core-modules/list.py
@@ -1,7 +1,7 @@
 from core.tui.components import (
                                 LOG,
                                 LOG_WARNING)
-from core.tui.colors import blue,green
+from core.tui.colors import blue,green,yellow
 import core.repositories
 from typing import List
 import argparse
@@ -16,7 +16,7 @@ def entrypoint(argv : List[str])->None:
 
     # add arguments
     parser.add_argument("-l","--inline",help="display the repositories inline, with minimal informations",action="store_true")
-    parser.add_argument("-t","--tags",help="filters the repositories by tags",type=str)
+    parser.add_argument("-t","--tags",help="filters the repositories by tags",nargs="*",type=str)
 
     # parse argv
     args = parser.parse_args(argv)
@@ -25,17 +25,21 @@ def entrypoint(argv : List[str])->None:
     repositories = core.repositories.get_repositories()
     repositories_names = list(repositories.keys())
 
+    if args.tags:
+        repositories_names = [repository_name for repository_name in repositories_names if repositories[repository_name]["tags"] and args.tags in repositories[repository_name]["tags"]]
+
     # init buffer
     buffer = ""
 
     # format output -> inline
     if args.inline:
         for repository_name in repositories_names:
-            tags =  [] if repositories[repository_name]["tags"] == None else repositories[repository_name]["tags"]
-            buffer += f'{blue(repository_name)} {repositories[repository_name]["origin"]} {blue("->")} {repositories[repository_name]["path"]} {green(" ".join(tags))}\n'
+            tags =  "" if repositories[repository_name]["tags"] == None else f'\n\ttags: {" ".join(repositories[repository_name]["tags"])}'
+            buffer += f'{blue(repository_name)}\n\torigin: {repositories[repository_name]["origin"]}\n\tpath: {repositories[repository_name]["path"]}\n\tauthor: {repositories[repository_name]["author"]}{tags}\n'
 
     # format output -> ls style                
     else:
-        pass
+        buffer = " \t".join(repositories_names)
 
     print(buffer)
+    LOG(f"Displaying {len(repositories_names)} repositories")
