b83e63f7b04240d2e85062057e873b16954468f4
rubenanko <rubenanko@gmail.com>
2025-10-14 12:44:16 +0200

    feat(): find module

--- src/core/git.py
+++ src/core/git.py
@@ -21,3 +21,8 @@ def get_author(repository_path: str)->str:
     return_code, author = exec("show | grep Author",repository_path,True)
     author = author.split(": ")[1].split(" <")[0]
     return author
+
+def get_origin(repository_path: str)->str:
+    return_code, origin = exec("remote -v | grep ^origin",repository_path,True)
+    origin = origin.split("origin\t")[1].split(" (fetch)")[0]
+    return origin
